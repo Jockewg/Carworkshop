5b7567702374ce9e48e96a10c2408869d880d267
Joakim Widerberg <Joakim_Widerberg@hotmail.com>
2015-02-23 17:23:51 +0100

    Create a simple login window with .CSS following a basic tutorial not using any fxml

--- CarWorkshop/src/main/java/com/joakimwiderberg/carworkshop/MainApp.java
+++ CarWorkshop/src/main/java/com/joakimwiderberg/carworkshop/MainApp.java
@@ -1,37 +1,81 @@
 package com.joakimwiderberg.carworkshop;
 
 import javafx.application.Application;
-import static javafx.application.Application.launch;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 
 public class MainApp extends Application {
 
     @Override
-    public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Scene.fxml"));
+    public void start(Stage primaryStage) throws Exception {
         
-        Scene scene = new Scene(root);
+        primaryStage.setTitle("CarWorkshop2015");
+        
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+        
+        Text scenetitle = new Text("Welcome");
+        scenetitle.setId("welcome-text");
+        grid.add(scenetitle, 0, 0, 2, 1);
+        
+        Label userName = new Label("User Name:");
+        grid.add(userName, 0, 1);
+        
+        TextField userTextField = new TextField();
+        grid.add(userTextField, 1, 1);
+        
+        Label pw = new Label("Password:");
+        grid.add(pw, 0, 2);
+        
+        PasswordField pwbox = new PasswordField();
+        grid.add(pwbox, 1, 2);
+        
+        //grid.setGridLinesVisible(true);
+        
+        Button btn = new Button("Sign in");
+        HBox hbBtn = new HBox(10);
+        hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
+        hbBtn.getChildren().add(btn);
+        grid.add(hbBtn, 1, 4);
+        
+        final Text actiontarget = new Text();
+        actiontarget.setId("actiontarget");
+        grid.add(actiontarget, 1, 6);
+        
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+            
+            @Override
+            public void handle(ActionEvent event) {
+                actiontarget.setText("Sing in Button pressed");
+            }
+            
+        });
+        
+        Scene scene = new Scene(grid, 300, 275);
+        primaryStage.setScene(scene);
+        
         scene.getStylesheets().add("/styles/Styles.css");
+        //Parent root = FXMLLoader.load(getClass().getResource("/fxml/Scene.fxml"));
         
-        stage.setTitle("JavaFX and Maven");
-        stage.setScene(scene);
-        stage.show();
+        primaryStage.show();
         
-        System.out.println("Hej");
     }
-
-    /**
-     * The main() method is ignored in correctly deployed JavaFX application.
-     * main() serves only as fallback in case the application can not be
-     * launched through deployment artifacts, e.g., in IDEs with limited FX
-     * support. NetBeans ignores main().
-     *
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         launch(args);
     }
